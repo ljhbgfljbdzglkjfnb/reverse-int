7bb4da5b961c412c430b38258b08eb286526d582
Olga <kartoshkaslukom@gmail.com>
2022-06-28 13:00:57 +0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    number = Math.abs(n);
+    text = number.toString();
+    array = text.split('').reverse();
+    res = Number(array.join(''));
+    return res;
 }
